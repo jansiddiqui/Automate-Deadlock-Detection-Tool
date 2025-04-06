3408862d54b2d5cb1dd231237de958393ad2c18d
Jan <jan810693@gmail.com>
2025-04-06 16:43:31 +0530

    updated

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -54,7 +55,5 @@ def check_deadlock():
         return jsonify({"message": "✅ No Deadlock Detected.", "deadlockPath": []})
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host="0.0.0.0", port=port)
